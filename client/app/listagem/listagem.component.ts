a3a86bc49ecd305652db584874a4eaed701ba9df
carolinamiss <carolina.missfeldt@gmail.com>
2017-12-14 10:44:32 -0200

    fadeOut no painel ao ser removido

--- client/app/listagem/listagem.component.ts
+++ client/app/listagem/listagem.component.ts
@@ -1,3 +1,4 @@
+import { PainelComponent } from './../painel/painel.component';
 import { Component, OnInit, Input } from '@angular/core';
 import { Http } from '@angular/http';
 import { FotoComponent } from './../foto/foto.component';
@@ -6,44 +7,48 @@ import { CadastroService } from './../cadastro/cadastro.service';
 @Component({
     moduleId: module.id,
     selector: 'listagem',
-    templateUrl: './listagem.component.html' 
+    templateUrl: './listagem.component.html'
 })
-export class ListagemComponent implements OnInit { 
+export class ListagemComponent implements OnInit {
 
     fotos: FotoComponent[] = [];
     mensagem: string = '';
     nomeFoto: string;
     foto: FotoComponent;
     id: string;
-    constructor( private cadastroService: CadastroService) {
-        
+    constructor(private cadastroService: CadastroService) {
+
     }
 
-    ngOnInit(){
-        this.listarFotos()    
+    ngOnInit() {
+        this.listarFotos()
     }
 
-    removerFoto(foto){
+    removerFoto(foto: FotoComponent, painel: PainelComponent) {
         this.cadastroService.remover(foto)
-        .subscribe(
+            .subscribe(
             success => {
-                let novasFotos = this.fotos.slice(0);
-                let indice = novasFotos.indexOf(foto);
-                novasFotos.splice(indice, 1);
-                this.fotos = novasFotos;
-        },
+                painel.fadeOut(() => {
+
+                    let novasFotos = this.fotos.slice(0);
+                    let indice = novasFotos.indexOf(foto);
+                    novasFotos.splice(indice, 1);
+                    this.fotos = novasFotos;
+                    this.mensagem = 'Foto removida com sucesso';
+                });
+            },
             error => {
                 this.mensagem = 'Não foi possível remover a foto'
             }
-        )
+            )
     }
 
     listarFotos() {
         this.cadastroService.listar()
-        .subscribe( fotos =>{
-            this.fotos = fotos
+            .subscribe(fotos => {
+                this.fotos = fotos
 
-        }, error => console.log(error))
+            }, error => console.log(error))
     }
 
 }
--- client/app/painel/painel.component.ts
+++ client/app/painel/painel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
 
 @Component({
     moduleId: module.id,
@@ -10,10 +10,19 @@ export class PainelComponent implements OnInit {
 
     @Input() titulo: string;
 
+    constructor(private elemento: ElementRef) {
+
+    }
+
     ngOnInit() {
         this.titulo = this.titulo.length > 7 ?
             this.titulo.substr(0, 7) + '...' :
             this.titulo;
+    }
+
+    fadeOut(cb) {
+        // erro de compilação! Não entra o $!
+        $(this.elemento.nativeElement).fadeOut("slow",cb);
     }
 
 }
